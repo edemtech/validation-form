956e3ba26330cf603d24cb221e740bb7e5d6d849
Edward Grukachev <e.grukachev@torgiclub.ru>
2017-08-10 13:48:05 +0300

    Validation over

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MaskedInput from 'react-maskedinput';
+
 import './App.css';
 
 class App extends React.Component {
@@ -9,54 +10,63 @@ class App extends React.Component {
             fio: '',
             email: '',
             phone: '',
-            validation: {
-                fio: true,
-                email: true,
-                phone: true
-            },
+            errorFields: [],
+            status: '',
         }
+        window.MyForm = this;
         this.submit = this.submit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
-    submit(e) {
-        e.preventDefault();
-        this.validate();
-    }
-    handleChange(e) {
-        this.setState({[e.target.name]: e.target.value});
-
-    }
     validate() {
-        let {fio, email, phone, validation} = this.state;
-        let isValid = {
-            fio: true,
-            email: true,
-            phone: true,
-        }
-        //validate fio
-        !fio.match(/^([A-zА-яЁё]+\s[A-zА-яЁё]+){2}$/) ? isValid.fio=false : isValid.fio=true;
-        //validate email
-        !email.match(/^[a-zA-Z0-9.-]+@(yandex.(ru|com|ua|kz|by)|ya.ru)$/) ? isValid.email=false : isValid.email=true;
-        //validate phone
+        let {fio, email, phone} = this.state;
+        let isValid = false,
+            errorFields = [];
+        if ( !fio.match(/^([A-zА-яЁё]+\s[A-zА-яЁё]+){2}$/) ) errorFields.push('fio');
+        if ( !email.match(/^[a-zA-Z0-9.-]+@(yandex.(ru|com|ua|kz|by)|ya.ru)$/) ) errorFields.push('email');
         let str = phone.match(/\d+/g, '')+'',
             s = str.split(',').join(''),
             phoneNumberSum = 0;
         for ( let item of s ) {
             phoneNumberSum += Number(item);
         }
-        isNaN(phoneNumberSum) || phone.includes('_') || phoneNumberSum > 30 ? isValid.phone=false : isValid.phone = true;
-        this.setState({validation: isValid})
-        console.log(validation);
-        // if ( valid.fio && valid.email && valid.phone ) {
-        //     this.setState({status: 'success'});
-        // } else this.setState({status: 'error'})
+        if ( isNaN(phoneNumberSum) || phone.includes('_') || phoneNumberSum > 30 ) errorFields.push('phone');
+        if ( errorFields.length === 0 ) isValid = true;
+        return {
+            isValid,
+            errorFields
+        }
+    }
+    getData() {
+        let obj = {}
+    }
+    setData(obj) {
+
+    }
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+    submit(e) {
+        e.preventDefault();
+        let validate = this.validate();
+        this.setState({errorFields: validate.errorFields})
+        if ( validate.isValid ) {
+            document.getElementById('submitButton').setAttribute('disabled', 'disabled');
+            fetch(e.currentTarget.action)
+                .then( response => {
+                    return response.json();
+                })
+                .then( json => {
+                    console.log(json);
+                    this.setState({status: json.status})
+                });
+        }
     }
     fioKeyUp(e) {
         e.currentTarget.value = e.currentTarget.value.replace(/[^a-zA-Za-яА-Я\s@]+/, '');
     }
 
     render() {
-        const { name, email, phone, validation } = this.state;
+        let { errorFields } = this.state;
         const resultContainer = (
             this.state.status === 'success' ?
                 <div className="alert alert-success">
@@ -70,14 +80,16 @@ class App extends React.Component {
                         </div> : ''
         )
         return (
-            <form className="col-sm-3 panel" action="URL.com/asdad/asdasd" onSubmit={this.submit}>
-                {resultContainer}
+            <form className="col-sm-3 panel" action="error.json" onSubmit={this.submit}>
+                <div id="resultContainer">
+                    {resultContainer}
+                </div>
                 <div className="form-group">
                     <label for="formGroupExampleInput">ФИО</label>
                     <input
                         type="text"
                         name="fio"
-                        className={"form-control "+(!validation.fio?'error':'')}
+                        className={"form-control "+(errorFields.includes('fio')?'error':'')}
                         placeholder="Введите ФИО"
                         value={this.state.fio}
                         onChange={this.handleChange}
@@ -88,7 +100,7 @@ class App extends React.Component {
                     <input
                         type="text"
                         name="email"
-                        className={"form-control "+(!validation.email?'error':'')}
+                        className={"form-control "+(errorFields.includes('email')?'error':'')}
                         placeholder="Ввведите почту"
                         value={this.state.email}
                         onChange={this.handleChange}/>
@@ -99,7 +111,7 @@ class App extends React.Component {
                         mask="+7(111)111-11-11"
                         type="text"
                         name="phone"
-                        className={"form-control "+(!validation.phone?'error':'')}
+                        className={"form-control "+(errorFields.includes('phone')?'error':'')}
                         placeholder="Введите номер"
                         value={this.state.phone}
                         onChange={this.handleChange}/>
